cf13eaf4ce00bef898d8c4e956d855b5417b0ef0
Rasheed Iskilu <riskilu@plateaumed.com>
2023-12-25 18:46:35 +0100

    slug

--- config/plugins.js
+++ config/plugins.js
@@ -13,4 +13,15 @@ module.exports = ( { env } ) => ( {
       },
     },
   },
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        article: {
+          field: 'slug',
+          references: 'title',
+        },
+      },
+    },
+  },
 } );
